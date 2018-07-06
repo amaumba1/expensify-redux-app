914c8a9cfe1656ffecf8a1fa1e286b244da829b2
Ally Maumba <amaumba1@gmail.com>
2018-07-06 00:47:20 -0500

    add store

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -4,23 +4,36 @@ import { createStore } from 'redux';
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
+      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1
+
       return {
-        count: state.count + 1
+        count: state.count + incrementBy
       }
     case 'DECREMENT':
+      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1
+
       return {
-        count: state.count - 1
+        count: state.count - decrementBy
+      }
+    case 'SET':
+      return {
+        count: action.count
       }
     case 'RESET':
       return {
-        count: state.count === 0
+        count: 0
       }
+    
     default:
       return state; 
   }
 })
 
-console.log(store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+})
+
+
 
 // Actions - than an object that gets sent to the store. 
 
@@ -32,8 +45,10 @@ console.log(store.getState());
 // dispatch job is to send action to the store. 
 
 store.dispatch({
-  type: 'INCREMENT'
+  type: 'INCREMENT',
+  incrementBy: 5
 })
+
 
 store.dispatch({
   type: 'INCREMENT'
@@ -49,8 +64,19 @@ store.dispatch({
   type: 'DECREMENT'
 })
 
+// decremenetBy 10 
+
+store.dispatch({
+  type: 'DECREMENT',
+  decrementBy: 10
+})
+
+store.dispatch({
+  type: 'SET',
+  count: 101
+})
 
 // I'd like to increment the count
 // I'd like to reset the count to zero
 
-console.log(store.getState());
+
